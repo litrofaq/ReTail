0f79f918377550c47e6a60baef8c0e1067c40680
litrofaq <kazymov.eugene@gmail.com>
2021-11-01 14:03:13 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReTail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int app = 100;
+            for (int i = 0; i < app; i++)
+            {
+                i++;
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
